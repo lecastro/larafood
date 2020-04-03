fe0351cbb6997aa2230de9bd4c67fdcf7a22db7b
lucas castro <lucas.castro1993@hotmail.com>
2020-04-03 12:13:05 -0300

    paginar os planos do larafood

--- app/Http/Controllers/Admin/PlansController.php
+++ app/Http/Controllers/Admin/PlansController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+
 
 class PlansController extends Controller
 {
+    private $repository;
+
+    public function __construct(Plan $plan)
+    {
+        $this->repository = $plan;    
+    }
+
     public function index()
     {
-        return view('admin.pages.plans');
+        return view('admin.pages.plans', ['plans' => $this->repository->get() ]);
     }
 }
--- resources/views/admin/pages/plans.blade.php
+++ resources/views/admin/pages/plans.blade.php
@@ -3,9 +3,39 @@
 @section('title', 'Planos')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+<h1>Planos</h1>
 @stop
 
 @section('content')
-    <p>Welcome in page plans.</p>
+<div class="card">
+    <div class="card-header">
+        #filtros
+    </div>
+    <div class="card-body">
+        <table class="table table-condensend">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            @foreach ($plans as $items)
+            <tr>
+                <td>
+                    {{ $items->name }}
+                </td>
+                <td>
+                    {{ $items->price }}
+                </td>
+                <td>
+                    <a href="#" class="btn btn-warning"></a>
+                </td>
+
+            </tr>
+
+            @endforeach
+        </table>
+    </div>
+</div>
 @stop
